67061c08f9870fb0d4a5b30776f29f5d0976954d
george <george>
2021-11-04 17:54:44 -0400

    support using pip to install soapyfile.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='soapyfile',
+    version='0.1.0',
+    author='George MagirosLiu',
+    author_email='gmagiros@gmail.com',
+    url='https://github.com/roseengineering/rfsoapyfile',
+    description='Record SDR to wav file using soapysdr library',
+    long_description='Record SDR to wav file using soapysdr library',
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+            'console_scripts': [ 'soapyfile.py = soapyfile:main' ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ),
+    keywords='soapyfile',
+    install_requires=[
+        "numpy",
+    ],
+    zip_safe=False
+)
